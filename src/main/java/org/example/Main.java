a503f5d9444f5b2d970029b4dac16142174fa9e8
TD9002 settings <srhgithub@mein.gmx>
2024-01-17 15:03:25 +0100

    ActivityDiagramm hinzugefügt

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,22 +14,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-     //   CSVReader.readCsv();
-        MySQLConnection mySQLConnection = new MySQLConnection(PropertyReader.readProperties());
-        System.out.println(mySQLConnection.getDbName());
-
-       // mySQLConnection.insertCountries(CSVReader.getListLocation());
-     //   mySQLConnection.insertCities(CSVReader.getListLocation());
-       // System.out.println(CSVReader.getListLocation().get(158).getCity());
-      //  mySQLConnection.csvToSql(CSVReader.getListLocation());
-      //  mySQLConnection.addCities(CSVReader.getListLocation());
-
-
-
-
-
-
-
 
 
 
